7a0f6ff24696058acd5ec8632be439ab3877bcad
sandeep reddy <sandeepreddyr12@gmail.com>
2023-06-10 11:13:23 +0530

    cleaned up comented code & formatted according to natspec style guide

--- contracts/Predictor.sol
+++ contracts/Predictor.sol
@@ -5,8 +5,9 @@ pragma solidity ^0.8.7;
 // enter the match(by paying min amount),
 // get the current matches from chianlink functions,
 //automate the excutions using chainlink keepers,
-// provide a option to transfer the amount to their accounts.
+// provide a option to withdraw the amount to their accounts.
 
+//imports
 import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
 /* Errors */
@@ -15,7 +16,15 @@ error Raffle__UpkeepNotNeeded();
 error Predictor__inValidContest();
 error Predictor__notEnoughBalance();
 
+/**@title sports predictor smartcontract
+ * @author Sandeep Reddy
+ * @notice This contract is for betting/predicting the results of the match.
+ * @dev This contract implements the Chainlink automations
+ * and Chainlink functions for fetching the live matches & its results.
+ */
+
 contract sportsPredictor is AutomationCompatibleInterface {
+
     /* Type declarations */
     enum result {
         won_by_teamA,
@@ -55,10 +64,10 @@ contract sportsPredictor is AutomationCompatibleInterface {
     uint256 private immutable i_entryFee;
 
     // chainlink automation varaiables
-
     uint256 private immutable interval;
     uint256 private lastTimeStamp;
 
+    /* Events */
     event Ongoing_Contests(
         uint256 matchId,
         string teamA,
@@ -71,18 +80,7 @@ contract sportsPredictor is AutomationCompatibleInterface {
         result contestResult
     );
 
-    // event ContestEntry(
-    //     address indexed contestant,
-    //     uint256 contestId,
-    //     string teamPicked
-    // );
-
-    // event ContestResult(
-    //     string contestName,
-    //     string winningTeam,
-    //     uint256 totalPayout
-    // );
-
+    /* Functions */
     constructor(uint256 entranceFee, uint256 updateInterval) {
         i_entryFee = entranceFee;
 
@@ -102,6 +100,7 @@ contract sportsPredictor is AutomationCompatibleInterface {
             Game(12124, "india", "england", matchStatus.Yet_to_Start)
         );
 
+        // Emit an event when contest is created
         emit Ongoing_Contests(
             12124,
             "india",
@@ -124,12 +123,25 @@ contract sportsPredictor is AutomationCompatibleInterface {
         lastTimeStamp = block.timestamp;
     }
 
+    /**
+     * @notice this randomNum function generates the random number(ofcourse it can be tampered) in a desired range.
+     * @dev This is the function intented to use for temporary purpose,
+     *  had faced issues while fetchings api from chainlink functions which is in closed beta.
+     * untill, random num generator replaces it.
+     */
+
     function randomNum(uint256 range) internal view returns (uint256) {
         return
             uint256(
                 keccak256(abi.encodePacked(block.difficulty, block.timestamp))
             ) % range;
     }
+
+    /**
+     * @notice this createContest fuction creates the contests automatically after fetching results from chainlink functions.
+     * @dev had faced issues while fetchings api from chainlink functions which is in closed beta.
+     * untill, hardcoded the contests and events.
+     */
 
     function createContest() private {
         uint256 randNum = randomNum(100000);
@@ -145,6 +157,14 @@ contract sportsPredictor is AutomationCompatibleInterface {
         );
     }
 
+    /**
+     * @dev This is the function that the Chainlink Keeper nodes call
+     * they look for `upkeepNeeded` to return True.
+     * the following should be true for this to return true:
+     * 1. The contract has ETH.
+     * 2. Implicity, your subscription is funded with LINK.
+     */
+
     function checkUpkeep(
         bytes memory /* checkData */
     )
@@ -156,6 +176,12 @@ contract sportsPredictor is AutomationCompatibleInterface {
         upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
         // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
     }
+    
+
+    /**
+     * @dev Once `checkUpkeep` is returning `true`, this function is called
+     * and it kicks off a Chainlink VRF call to get a random winner.
+     */
 
     function performUpkeep(bytes calldata /* performData */) external override {
         //We highly recommend revalidating the upkeep in the performUpkeep function
@@ -163,15 +189,9 @@ contract sportsPredictor is AutomationCompatibleInterface {
         if (!upkeepNeeded) {
             revert Raffle__UpkeepNotNeeded();
         }
-        // require(upkeepNeeded, "Time interval not met");
 
-        //not needed for a moment
-        // for (uint i = 0; i < ContestsId.length; i++) {
-        //     if (ContestsId[i].status == matchStatus.Finished) {
-        //         ContestsId[i] = ContestsId[ContestsId.length - 1];
-        //         ContestsId.pop();
-        //     }
-        // }
+        // this loop currently hardcoded to chage the results for given time interval
+        // gonna replace it with chainlink functions
 
         for (uint i = 0; i < ContestsId.length; i++) {
             if (ContestsId[i].status == matchStatus.in_Progress) {
@@ -193,6 +213,10 @@ contract sportsPredictor is AutomationCompatibleInterface {
         // We don't use the performData in this example. The performData is generated by the Automation Node's call to your checkUpkeep function
     }
 
+    /**
+     * @dev isValidContest is to check the contest is valid.
+     */
+
     function isValidContest(uint256 _contestId) private view returns (bool) {
         for (uint256 i = 0; i < ContestsId.length; i++) {
             if (
@@ -204,6 +228,12 @@ contract sportsPredictor is AutomationCompatibleInterface {
         }
         return false;
     }
+
+    /**
+     * @notice enterContest function is let you enter the contest.
+     * @dev This is the function which let players enter the contest with minimum of enterance fee.
+     * player has ability to place bet on both the teams, his stake is added accordingly
+     */
 
     function enterContest(
         uint256 _contestId,
@@ -232,6 +262,13 @@ contract sportsPredictor is AutomationCompatibleInterface {
         }
     }
 
+    /**
+     * @notice declareResult function is used to declare the result of the contest.
+     * @dev This is the fake result simulater function ,
+     *  which declares the result of the contest on random basis.
+     * --- usually the results fetched from offchain functions.
+     */
+
     function declareResult(uint256 _contestId) private {
         uint256 randNum = randomNum(3);
         ContestDetails storage contest = contestEntrances[_contestId];
@@ -259,6 +296,14 @@ contract sportsPredictor is AutomationCompatibleInterface {
             );
         }
     }
+
+    /**
+     * @notice fetchBalance function is used to fetch the balance from winnings.
+     * @dev this function calculates the winnings from finished matches.
+     * -- the logic "(playerStake.teamA * contest.totalAmount_B) / contest.totalAmount_A", gives the winning stake/percentage of TeamA-player in total amount of TeamB.
+     * by adding above result with "playerStake.teamA" gives the total winnings of player who placed bet on teamA.
+     * for contest resulting Draw/no_result, the placed bets add to winnings accordingly.
+     */
 
     function fetchBalance() public returns (uint256) {
         uint256 totalWinnings;
@@ -306,7 +351,7 @@ contract sportsPredictor is AutomationCompatibleInterface {
 
         Winnings[msg.sender] = 0;
 
-        payable(msg.sender).transfer(Winnings[msg.sender]);
+        payable(msg.sender).transfer(balance);
         // with transfer, if transaction fails, it is automatically reverted.
         //incase of send or call(returns bool), we have to call require expictly.
     }
@@ -320,6 +365,10 @@ contract sportsPredictor is AutomationCompatibleInterface {
     function getBalance() public view returns (uint256) {
         return Winnings[msg.sender];
     }
+
+/**
+     * @notice getContestStake function gives the placed bet/stake of player. 
+     */
 
     function getContestStake(
         uint256 _contestId,
@@ -339,17 +388,17 @@ contract sportsPredictor is AutomationCompatibleInterface {
     }
 
     // function getTotalAmount(
-    //     string memory _contestName
+    //    uint256 _contestId,
     // ) public view returns (uint256, uint256) {
-    //     ContestDetails storage contest = contestEntrances[_contestName];
+    //     ContestDetails storage contest = contestEntrances[_contestId];
     //     return (contest.totalAmount_A, contest.totalAmount_B);
     // }
 
     // function getPlayerStake(
-    //     string memory _contestName,
+    //     uint256 _contestId,
     //     address _player
     // ) public view returns (uint256, uint256) {
-    //     ContestDetails storage contest = contestEntrances[_contestName];
+    //     ContestDetails storage contest = contestEntrances[_contestId];
     //     PlayerStake storage playerStake = contest.playerStakes[_player];
     //     return (playerStake.teamA, playerStake.teamB);
     // }
