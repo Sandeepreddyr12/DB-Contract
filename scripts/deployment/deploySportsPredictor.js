6793fada2e1c02a4808e8b0b36005d94cbc470aa
sandeep reddy <sandeepreddyr12@gmail.com>
2023-07-30 20:02:00 +0530

    changed eth value and arguments

--- scripts/deployment/deploySportsPredictor.js
+++ scripts/deployment/deploySportsPredictor.js
@@ -1,41 +1,41 @@
-const { ethers, network, run } = require("hardhat")
+const { ethers, network, run } = require("hardhat");
 const {
     VERIFICATION_BLOCK_CONFIRMATIONS,
     networkConfig,
     developmentChains,
-} = require("../../helper-hardhat-config")
+} = require("../../helper-hardhat-config");
 
 async function deploySportsPredictor() {
-    const chainId = network.config.chainId
+    const chainId = network.config.chainId;
     console.log(chainId);
-    const raffleEntranceFee = ethers.utils.parseEther("0.01")
-    const automationUpdateInterval = networkConfig[chainId]["automationUpdateInterval"]
+    const raffleEntranceFee = ethers.utils.parseEther("0.01");
+    // const automationUpdateInterval = networkConfig[chainId]["automationUpdateInterval"];
 
-    const sportsPredictorFactory = await ethers.getContractFactory("sportsPredictor")
+    const sportsPredictorFactory = await ethers.getContractFactory("sportsPredictor");
     const sportsPredictor = await sportsPredictorFactory.deploy(
         raffleEntranceFee,
-        automationUpdateInterval
-    )
+        // automationUpdateInterval
+    );
 
     const waitBlockConfirmations = developmentChains.includes(network.name)
         ? 1
-        : VERIFICATION_BLOCK_CONFIRMATIONS
-    await sportsPredictor.deployTransaction.wait(waitBlockConfirmations)
+        : VERIFICATION_BLOCK_CONFIRMATIONS;
+    await sportsPredictor.deployTransaction.wait(waitBlockConfirmations);
 
-    console.log(`sportsPredictor deployed to ${sportsPredictor.address} on ${network.name}`)
+    console.log(`sportsPredictor deployed to ${sportsPredictor.address} on ${network.name}`);
 
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         await run("verify:verify", {
             address: sportsPredictor.address,
-            constructorArguments: [automationUpdateInterval],
-        })
+            constructorArguments: [raffleEntranceFee],
+        });
     }
 }
 
 deploySportsPredictor().catch((error) => {
-    console.error(error)
-    process.exitCode = 1
-})
+    console.error(error);
+    process.exitCode = 1;
+});
 
 // module.exports = {
 //     deploySportsPredictor,
